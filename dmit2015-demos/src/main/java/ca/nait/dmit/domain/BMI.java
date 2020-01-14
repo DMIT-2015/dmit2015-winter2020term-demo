90f5718723d0e65c2e908285df60ce7ee0858bfd
Sam Wu <swu@nait.ca>
2020-01-14 14:08:15 -0700

    updated soltion to ChineseZodiacTest.java

--- dmit2015-demos/src/main/java/ca/nait/dmit/domain/BMI.java
+++ dmit2015-demos/src/main/java/ca/nait/dmit/domain/BMI.java
@@ -1,33 +1,44 @@
 package ca.nait.dmit.domain;
 
+/**
+ * This class is use to calculate a person's body mass index (BMI) and their BMI
+ * Category.
+ * 
+ * @author yourFirstName yourLastName
+ * @version 2015.01.16
+ */
 public class BMI {
 
 	// Declare data fields to store data
 	private String name;
 	private int weight;
 	private int height;
-	
+
 	// Encapsulate access to the data fields using getters/setters
 
 	public String getName() {
 		return name;
 	}
+
 	public void setName(String name) {
 		this.name = name;
 	}
+
 	public int getWeight() {
 		return weight;
 	}
+
 	public void setWeight(int weight) {
 		this.weight = weight;
 	}
+
 	public int getHeight() {
 		return height;
 	}
+
 	public void setHeight(int height) {
 		this.height = height;
 	}
-	
 
 	// Declare constructor to initialize the object
 	public BMI(String name, int weight, int height) {
@@ -36,13 +47,51 @@ public class BMI {
 		this.weight = weight;
 		this.height = height;
 	}
-	
-	
+
 	// Declare instance-level methods that performs operations using the data fields
+	/**
+	 * Calculate the body mass index (BMI) using the weight and height of the
+	 * person. The BMI of a person is calculated using the formula: BMI = 700 *
+	 * weight / (height * height) where weight is in pounds and height is in inches.
+	 * 
+	 * @return the body mass index (BMI) value of the person
+	 */
 	public double getBMI() {
 		return 703 * weight / Math.pow(height, 2);
 	}
-	
+
+	/**
+	 * Determines the BMI Category of the person using their BMI value and comparing
+	 * it against the following table.
+	 * <table>
+	 * <thead>
+	 * <tr>
+	 * <th>BMI Range</th>
+	 * <th>BMI Category</th>
+	 * </tr>
+	 * </thead> <tbody>
+	 * <tr>
+	 * <td>< 18.5</td>
+	 * <td>underweight</td>
+	 * </tr>
+	 * <tr>
+	 * <td>>= 18.5 and < 25</td>
+	 * <td>normal</td>
+	 * </tr>
+	 * <tr>
+	 * <td>>= 25 and < 30</td>
+	 * <td>overweight</td> *
+	 * </tr>
+	 * <tr>
+	 * <td>>= 30</td>
+	 * <td>obese</td>
+	 * </tr>
+	 * </tbody>
+	 * </table>
+	 *
+	 * @return one of following: underweight, normal, overweight, obese.
+	 */
+
 	public String getCategory() {
 		String category = "obese";
 		final double bmiValue = getBMI();
@@ -57,5 +106,5 @@ public class BMI {
 		}
 		return category;
 	}
-	
+
 }
--- dmit2015-demos/src/test/java/ca/nait/dmit/domain/ChineseZodiacTest.java
+++ dmit2015-demos/src/test/java/ca/nait/dmit/domain/ChineseZodiacTest.java
@@ -14,14 +14,14 @@ class ChineseZodiacTest {
 		assertEquals("Bob Jones", chineseZodiac1.getName());
 		assertEquals("Rat", chineseZodiac1.getAnimal());
 	}
-	
+
 	@Test
 	void shouldMidcyleYear() {
 		ChineseZodiac chineseZodiac1 = new ChineseZodiac("Michael Smith", 1958);
 		assertEquals("Michael Smith", chineseZodiac1.getName());
 		assertEquals("Dog", chineseZodiac1.getAnimal());
 	}
-	
+
 	@Test
 	void shouldBeCurrentYear() {
 		int currentYear = LocalDate.now().getYear();
@@ -30,4 +30,125 @@ class ChineseZodiacTest {
 		assertEquals("Rat", chineseZodiac1.getAnimal());
 	}
 
+	public static final int CURRENT_YEAR = LocalDate.now().getYear();
+
+	@Test
+	public void testGetAnimalRat() {
+		int animalYear = 1900;
+		while (animalYear <= CURRENT_YEAR) {
+			ChineseZodiac chineseZodiac1 = new ChineseZodiac("Bob Jones", animalYear);
+			assertEquals("rat", chineseZodiac1.getAnimal().toLowerCase());
+			animalYear += 12;
+		}
+	}
+
+	@Test
+	public void testGetAnimalOx() {
+		int animalYear = 1901;
+		while (animalYear <= CURRENT_YEAR) {
+			ChineseZodiac chineseZodiac1 = new ChineseZodiac("Bob Jones", animalYear);
+			assertEquals("ox", chineseZodiac1.getAnimal().toLowerCase());
+			animalYear += 12;
+		}
+	}
+
+	@Test
+	public void testGetAnimalTiger() {
+		int animalYear = 1902;
+		while (animalYear <= CURRENT_YEAR) {
+			ChineseZodiac chineseZodiac1 = new ChineseZodiac("Bob Jones", animalYear);
+			assertEquals("tiger", chineseZodiac1.getAnimal().toLowerCase());
+			animalYear += 12;
+		}
+	}
+
+	@Test
+	public void testGetAnimalRabbit() {
+		int animalYear = 1903;
+		while (animalYear <= CURRENT_YEAR) {
+			ChineseZodiac chineseZodiac1 = new ChineseZodiac("Bob Jones", animalYear);
+			assertEquals("rabbit", chineseZodiac1.getAnimal().toLowerCase());
+			animalYear += 12;
+		}
+	}
+
+	@Test
+	public void testGetAnimalDragon() {
+		int animalYear = 1904;
+		while (animalYear <= CURRENT_YEAR) {
+			ChineseZodiac chineseZodiac1 = new ChineseZodiac("Bob Jones", animalYear);
+			assertEquals("dragon", chineseZodiac1.getAnimal().toLowerCase());
+			animalYear += 12;
+		}
+	}
+
+	@Test
+	public void testGetAnimalSnake() {
+		int animalYear = 1905;
+		while (animalYear <= CURRENT_YEAR) {
+			ChineseZodiac chineseZodiac1 = new ChineseZodiac("Bob Jones", animalYear);
+			assertEquals("snake", chineseZodiac1.getAnimal().toLowerCase());
+			animalYear += 12;
+		}
+	}
+
+	@Test
+	public void testGetAnimalHorse() {
+		int animalYear = 1906;
+		while (animalYear <= CURRENT_YEAR) {
+			ChineseZodiac chineseZodiac1 = new ChineseZodiac("Bob Jones", animalYear);
+			assertEquals("horse", chineseZodiac1.getAnimal().toLowerCase());
+			animalYear += 12;
+		}
+	}
+
+	@Test
+	public void testGetAnimalGoat() {
+		int animalYear = 1907;
+		while (animalYear <= CURRENT_YEAR) {
+			ChineseZodiac chineseZodiac1 = new ChineseZodiac("Bob Jones", animalYear);
+			assertEquals("goat", chineseZodiac1.getAnimal().toLowerCase());
+			animalYear += 12;
+		}
+	}
+
+	@Test
+	public void testGetAnimalMonkey() {
+		int animalYear = 1908;
+		while (animalYear <= CURRENT_YEAR) {
+			ChineseZodiac chineseZodiac1 = new ChineseZodiac("Bob Jones", animalYear);
+			assertEquals("monkey", chineseZodiac1.getAnimal().toLowerCase());
+			animalYear += 12;
+		}
+	}
+
+	@Test
+	public void testGetAnimalRooster() {
+		int animalYear = 1909;
+		while (animalYear <= CURRENT_YEAR) {
+			ChineseZodiac chineseZodiac1 = new ChineseZodiac("Bob Jones", animalYear);
+			assertEquals("rooster", chineseZodiac1.getAnimal().toLowerCase());
+			animalYear += 12;
+		}
+	}
+
+	@Test
+	public void testGetAnimalDog() {
+		int animalYear = 1910;
+		while (animalYear <= CURRENT_YEAR) {
+			ChineseZodiac chineseZodiac1 = new ChineseZodiac("Bob Jones", animalYear);
+			assertEquals("dog", chineseZodiac1.getAnimal().toLowerCase());
+			animalYear += 12;
+		}
+	}
+
+	@Test
+	public void testGetAnimalPig() {
+		int animalYear = 1911;
+		while (animalYear <= CURRENT_YEAR) {
+			ChineseZodiac chineseZodiac1 = new ChineseZodiac("Bob Jones", animalYear);
+			assertEquals("pig", chineseZodiac1.getAnimal().toLowerCase());
+			animalYear += 12;
+		}
+	}
 }
