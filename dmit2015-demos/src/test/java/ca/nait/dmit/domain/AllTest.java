d881dba98de1ac6b97e25202c507355b98146e0d
Sam Wu <swu@nait.ca>
2020-02-03 08:47:18 -0700

    Add JUnit Test Suite

---
+++ dmit2015-demos/src/test/java/ca/nait/dmit/domain/AllTest.java
@@ -0,0 +1,14 @@
+package ca.nait.dmit.domain;
+
+import org.junit.platform.runner.JUnitPlatform;
+import org.junit.platform.suite.api.*;
+import org.junit.runner.RunWith;
+
+@RunWith(JUnitPlatform.class)
+@SuiteDisplayName("JUnit 5 Suite Demo")
+@SelectPackages("ca.nait.dmit.domain")
+//@SelectClasses({CircleTest.class, RectangleTest.class})
+
+public class AllTest {
+
+}
